df4c12db7e4f5a4d3b20ce680fb05470f5fe0b3a
Polly Orlowska <polly.orlowska@thetrainile.com>
2025-03-31 11:16:59 +0100

    add data class

--- app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -16,23 +16,23 @@
 package com.example.affirmations.data
 
 import com.example.affirmations.R
-import com.example.affirmations.model.Affirmation
+import com.example.affirmations.model.Affiramtion
 
 /**
- * [Datasource] generates a list of [Affirmation]
+ * [Datasource] generates a list of [Affiramtion]
  */
 class Datasource() {
-    fun loadAffirmations(): List<Affirmation> {
-        return listOf<Affirmation>(
-            Affirmation(R.string.affirmation1, R.drawable.image1),
-            Affirmation(R.string.affirmation2, R.drawable.image2),
-            Affirmation(R.string.affirmation3, R.drawable.image3),
-            Affirmation(R.string.affirmation4, R.drawable.image4),
-            Affirmation(R.string.affirmation5, R.drawable.image5),
-            Affirmation(R.string.affirmation6, R.drawable.image6),
-            Affirmation(R.string.affirmation7, R.drawable.image7),
-            Affirmation(R.string.affirmation8, R.drawable.image8),
-            Affirmation(R.string.affirmation9, R.drawable.image9),
-            Affirmation(R.string.affirmation10, R.drawable.image10))
+    fun loadAffirmations(): List<Affiramtion> {
+        return listOf<Affiramtion>(
+            Affiramtion(R.string.affirmation1, R.drawable.image1),
+            Affiramtion(R.string.affirmation2, R.drawable.image2),
+            Affiramtion(R.string.affirmation3, R.drawable.image3),
+            Affiramtion(R.string.affirmation4, R.drawable.image4),
+            Affiramtion(R.string.affirmation5, R.drawable.image5),
+            Affiramtion(R.string.affirmation6, R.drawable.image6),
+            Affiramtion(R.string.affirmation7, R.drawable.image7),
+            Affiramtion(R.string.affirmation8, R.drawable.image8),
+            Affiramtion(R.string.affirmation9, R.drawable.image9),
+            Affiramtion(R.string.affirmation10, R.drawable.image10))
     }
 }
